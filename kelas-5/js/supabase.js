1bf8bf9df542bbd2d6531ee4ac5dae7b8f9c31cd
sdn160gresik <sdn160gresik.1979@gmail.com>
2026-08-13 15:35:31 +0700

    Create supabase.js

---
+++ kelas-5/js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://dnasyoyvnzphbmuhihan.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_zQqU29LLgS2i0J50XJAM0w_N7coZMHf";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
